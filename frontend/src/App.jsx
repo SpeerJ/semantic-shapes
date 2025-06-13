4913ae971436a0fb02fe603cf4a80f04d184de31
Johann Speer <johann.speer@proton.me>
2025-06-13 20:55:06 -0300

    Frontend: Improved and styled examples

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,14 +13,16 @@ function App() {
                         This application demonstrates vector arithmetic on word embeddings.
                         Inspired by Word2Vec's ability to solve analogies like "king - man + woman = queen",
                         you can enter similar expressions below to find words closest to the resulting vector.
+                    </div>
+                    <VectorArithmetic/>
+                    <div style={{textAlign: 'left', marginTop: '20px'}}>
                         Some interesting examples:
                         <ul>
                             <li>"animal + culpable ≈ human"</li>
                             <li>"tokyo - japan + france ≈ paris"</li>
-                            <li>"king - man + woman = prince"</li>
+                            <li>"walking - walk + swim ≈ swimming"</li>
                         </ul>
                     </div>
-                    <VectorArithmetic/>
                 </div>
             </main>
         </div>
